801df1ceba383484011e4414d86b86778ddd5c50
Dianne Rees <drees3271@verizon.net>
2019-04-14 14:41:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function happyHolidaysTo (name) {
 
   return ('Happy holidays ${name}!')
 }
+var name = "Dianne";
 function happyHolidayTo (holiday, name) {
 
 return ('Happy ${holiday}, ${name}!')
@@ -13,6 +14,6 @@ return ('Happy ${holiday}, ${name}!')
 function holidayCountdown (holiday, days) {
   return ('It is ${days} until ${holiday},!')
 }
-var name = "you";
+
 var holiday = "solstice";
 var days = "13"
